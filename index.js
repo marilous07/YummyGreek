b5b7979b42dc1c74c3ee3cd8b3ae088ae8b50c3b
marilous07 <marilous07.mm@gmail.com>
2023-09-14 22:22:29 +0300

    2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const apiRoutes = require('./routes/api');
+
 const app = express();
 const port = 3000;
+
+app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
